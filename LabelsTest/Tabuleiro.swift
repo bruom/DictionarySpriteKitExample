2c573add438a6239fa94b722b1c7e09a6f108e6e
Bruno Omella Mainieri <bruno.omella@hotmail.com>
2015-05-15 13:47:50 -0300

    Removido navigation

---
+++ LabelsTest/Tabuleiro.swift
@@ -0,0 +1,13 @@
+//
+//  Tabuleiro.swift
+//  LabelsTest
+//
+//  Created by Bruno Omella Mainieri on 5/15/15.
+//  Copyright (c) 2015 BEPiD. All rights reserved.
+//
+
+import SpriteKit
+
+class Tabuleiro: SKNode {
+    
+}
